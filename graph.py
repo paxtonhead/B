62ac8018b1984ad7f675c07b9767faf0beb8cd66
Scott Murphy <moramurph@gmail.com>
2018-03-24 13:33:31 -0400

    Update graph.py

--- graph.py
+++ graph.py
@@ -105,50 +105,69 @@ class Graph:
     return self.connectedComponents
 
 
-  def fillOrder(self,v,visited, stack):
-    # Mark the current node as visited 
-    visited[v]= True
-    #Recur for all the vertices adjacent to this vertex
-    for i in self.graph[v]:
-        if visited[i]==False:
-            self.fillOrder(i, visited, stack)
-    stack = stack.append(v)
-     
- 
-    # Function that returns reverse (or transpose) of this graph
-  def getTranspose(self):
-    g = Graph(self.V)
-
-    # Recur for all the vertices adjacent to this vertex
-    for i in self.graph:
-        for j in self.graph[i]:
-            g.addEdge(j,i)
-    return g
-
-
   # The main function that finds and prints all strongly
   # connected components
   def calcSCCs(self):
+    
+    
+    # Function that returns reverse (or transpose) of this graph
+    def getTranspose():
+      g = Graph(self.V)
 
-      stack = []
-      # Mark all the vertices as not visited (For first DFS)
-      visited = [False]*(self.V)
-      # Fill vertices in stack according to their finishing
-      # times
-      for i in range(self.V):
+      # Recur for all the vertices adjacent to this vertex
+      for i in self.graph:
+          for j in self.graph[i]:
+              g.addEdge(j,i)
+      return g
+
+    
+    def fillOrder(v,visited, stack):
+      # Mark the current node as visited 
+      visited[v]= True
+      #Recur for all the vertices adjacent to this vertex
+      for i in self.graph[v]:
           if visited[i]==False:
-              self.fillOrder(i, visited, stack)
+              fillOrder(i, visited, stack)
+      stack = stack.append(v)
+    
+    
+    # A function used by DFS
+    def DFSUtil(v,visited):
+      # Mark the current node as visited and print it
+      visited[v]= True
+      self.connectedComponents[self.counter].add(v)
+      #Recur for all the vertices adjacent to this vertex
+      for i in self.graph[v]:
+        if visited[i]==False:
+          DFSUtil(i,visited) 
 
-      # Create a reversed graph
-      gr = self.getTranspose()
-        
-      # Mark all the vertices as not visited (For second DFS)
-      visited =[False]*(self.V)
-
-      # Now process all vertices in order defined by Stack
-      while stack:
-          i = stack.pop()
-          if visited[i]==False:
-              self.connectedComponents = gr.DFSUtil(i, visited)
-
+      self.counter += 1
       return self.connectedComponents
+    
+    
+    
+    
+    stack = []
+    # Mark all the vertices as not visited (For first DFS)
+    visited = [False]*(self.V)
+    # Fill vertices in stack according to their finishing
+    # times
+    for i in range(self.V):
+        if visited[i]==False:
+            fillOrder(i, visited, stack)
+
+    # Create a reversed graph
+    gr = getTranspose() 
+      
+    # Mark all the vertices as not visited (For second DFS)
+    visited =[False]*(self.V)
+
+    # Now process all vertices in order defined by Stack
+    while stack:
+        i = stack.pop()
+        if visited[i]==False:
+            self.connectedComponents = gr.DFSUtil(i, visited)
+
+    return self.connectedComponents
+
+
