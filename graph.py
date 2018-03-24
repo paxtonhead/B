1eb52b3bc3612c13047938e3bd846b33c44d9abd
Scott Murphy <moramurph@gmail.com>
2018-03-24 11:24:15 -0400

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,28 +1,96 @@
 from collections import defaultdict
 
-class Graph:
-
-  connectedComponents = defaultdict(set)
+class Graph: 
 
   def __init__(self, ver):
     self.V = ver
     self.graph = defaultdict(set)
     self.counter = 0
+    self.connectedComponents = defaultdict(set)
+    self.userBase = []
 
   def addEdge(self, u, v):
     self.graph[u].add(v)
 
+  def setUserBase(self, ub):
+    self.userBase = ub
+
+  def getUserBase(self):
+    return self.userBase
 
   def printGraph(self):
     print(self.graph)
 
-  def printConnected(self):
+  def printuserBase(self):
+    print(self.userBase)
+
+  def printConnectedComponents(self):
     print(self.connectedComponents)
 
-  def getConnectedComponents(self):
+  def getConnectedComponents(self): 
     return self.connectedComponents
 
+
+  def fillGraph(self):
+    for i in self.userBase: 
+      for user in self.userBase:
+
+      # Continues to avoid comparing user to themselves
+        if user is i:
+          continue
+
+        for wants in i['WANT']:
+          for haves in user['HAVE']:
+            if wants == haves:
+              self.addEdge(i['ID'], user['ID'])
+
+
+  def convertIDToUsername(self, D):
+    for key, value in D.items():
+    
+      result = set()
+
+      for users in self.userBase:
+        for i in value:
+          if users['ID'] == i:
+            result.add(users['USERNAME'])
+
+      D[key] = result
+
+    return D
+
   
+  def findWholeMatch(self, initialUser):
+    initialUser = self.userBase[0]
+    half_match = []
+    full_match = {}
+
+    # Initial search finds a half match
+    for user in self.userBase:
+
+      # Continues to avoid comparing user to themselves
+      if user == initialUser:
+        continue
+
+      for wants in initialUser['WANT']:
+        for haves in user['HAVE']:
+          if wants == haves:
+            half_match.append(user)
+            full_match['RECEIVE'] = wants
+
+
+    # Next searches the half matches the determine if there if a full match
+    for user in half_match:
+      
+      for haves in initialUser['HAVE']:
+        for wants in user['WANT']:
+          if wants == haves:
+            full_match['SEND'] = wants
+            full_match['NAME OF USER'] = user['USERNAME']
+
+    return full_match
+
+
   # A function used by DFS
   def DFSUtil(self,v,visited):
     # Mark the current node as visited and print it
@@ -34,6 +102,7 @@ class Graph:
         self.DFSUtil(i,visited) 
 
     self.counter += 1
+    return self.connectedComponents
 
 
   def fillOrder(self,v,visited, stack):
@@ -80,6 +149,6 @@ class Graph:
       while stack:
           i = stack.pop()
           if visited[i]==False:
-              gr.DFSUtil(i, visited)
+              self.connectedComponents = gr.DFSUtil(i, visited)
 
       return self.connectedComponents
